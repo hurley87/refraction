2737514bc937bb54f6c89131bda1e84376c31ccb
David Hurley <dhurls99@gmail.com>
2024-09-13 12:55:20 -0400

    new height

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container relative grid-cols-1 h-56 lg:h-screen flex-col items-center justify-center md:grid w-full lg:grid-cols-2 lg:px-0">
+    <div className="container relative grid-cols-1 h-40 lg:h-screen flex-col items-center justify-center md:grid w-full lg:grid-cols-2 lg:px-0">
       <div className="relative h-full flex-col bg-muted p-6 text-white dark:border-r flex">
         <div className="absolute inset-0 bg-zinc-900" />
         <div className="relative z-20 flex items-center text-lg font-bold">
