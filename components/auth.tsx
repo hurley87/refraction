1d39316347bae83b6f763a57ef5abb83b6be9d92
David Hurley <dhurls99@gmail.com>
2024-10-28 20:47:32 -0400

    add linkEmail to auth

--- components/auth.tsx
+++ components/auth.tsx
@@ -8,11 +8,31 @@ interface AuthProps {
 }
 
 export default function Auth({ children }: AuthProps) {
-  const { user, login, ready } = usePrivy();
+  const { user, login, ready, linkEmail } = usePrivy();
   const address = user?.wallet?.address as `0x${string}`;
 
   if (!ready) {
     return <div>Loading...</div>;
+  }
+
+  if (ready && user && !user.email) {
+    return (
+      <div className="flex flex-col gap-12">
+        <div>
+          <Button
+            className="bg-yellow-500 hover:bg-yellow-400 text-black"
+            size="lg"
+            onClick={linkEmail}
+          >
+            Link Email
+          </Button>
+        </div>
+        <p className="text-xs md:text-sm italic">
+          Claim your free commemorative mint and take an early spot in line for
+          the release.
+        </p>
+      </div>
+    );
   }
 
   if (ready && !address) {
