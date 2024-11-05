3cb43f746bd47199bfaf2d664e78f4b598423628
David Hurley <dhurls99@gmail.com>
2024-11-05 10:50:03 -0500

    remove iyk url verification

--- app/checkpoints/[id]/page.tsx
+++ app/checkpoints/[id]/page.tsx
@@ -2,33 +2,33 @@ import Checkpoint from "@/components/checkpoint";
 
 interface CheckpointPageProps {
   params: { id: string };
-  searchParams: { iykRef?: string };
+  // searchParams: { iykRef?: string };
 }
 
 export default async function CheckpointPage({
   params,
-  searchParams,
-}: CheckpointPageProps) {
+}: // searchParams,
+CheckpointPageProps) {
   const id = params.id as `0x${string}`;
-  const iykRef = searchParams.iykRef;
+  // const iykRef = searchParams.iykRef;
 
-  const NoAccess = () => (
-    <div className="absolute top-0 left-0 w-full h-screen flex justify-center items-center text-center z-50">
-      No access
-    </div>
-  );
+  // const NoAccess = () => (
+  //   <div className="absolute top-0 left-0 w-full h-screen flex justify-center items-center text-center z-50">
+  //     No access
+  //   </div>
+  // );
 
-  if (!iykRef) {
-    return <NoAccess />;
-  }
+  // if (!iykRef) {
+  //   return <NoAccess />;
+  // }
 
-  const response = await fetch(`https://api.iyk.app/refs/${iykRef}`);
+  // const response = await fetch(`https://api.iyk.app/refs/${iykRef}`);
 
-  const { isValidRef } = await response.json();
+  // const { isValidRef } = await response.json();
 
-  if (!isValidRef) {
-    return <NoAccess />;
-  }
+  // if (!isValidRef) {
+  //   return <NoAccess />;
+  // }
 
   return <Checkpoint id={id} />;
 }
