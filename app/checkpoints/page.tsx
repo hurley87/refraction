906642716f93c0eb01e516cafa9495b418a9e820
David Hurley <dhurls99@gmail.com>
2024-11-06 12:36:33 -0500

    move map and update copy

--- app/checkpoints/page.tsx
+++ app/checkpoints/page.tsx
@@ -9,6 +9,16 @@ export default async function CheckpointsPage() {
           <Checkpoints />
         </div>
       </div>
+      <div className="relative  flex flex-col gap-3 bg-gradient-to-r from-yellow-300 from-10% via-rose-300 via-30% to-sky-500 to-90% p-6 text-BLACK dark:border-r justify-between">
+        <div className="flex justify-center p-6">
+          <Image
+            src="/images/$IRL_VENUE MAP_2ND FLOOR_TRANSPARENT.png"
+            width={1000}
+            height={1000}
+            alt="venue map 2nd floor"
+          />
+        </div>
+      </div>
       <div className="relative  flex flex-row  bg-gradient-to-r from-orange-600 from-10% via-rose-300 via-90% to-yellow-300 to-100% p-6 text-BLACK dark:border-r justify-between">
         <div className="flex">
           <div className="flex-auto text-black text-lg p-6">
@@ -71,16 +81,7 @@ export default async function CheckpointsPage() {
           />
         </div>
       </div>
-      <div className="relative  flex flex-col gap-3 bg-gradient-to-r from-yellow-300 from-10% via-rose-300 via-30% to-sky-500 to-90% p-6 text-BLACK dark:border-r justify-between">
-        <div className="flex justify-center p-6">
-          <Image
-            src="/images/$IRL_VENUE MAP_2ND FLOOR_TRANSPARENT.png"
-            width={1000}
-            height={1000}
-            alt="venue map 2nd floor"
-          />
-        </div>
-      </div>
+
       <div className="relative  flex flex-col gap-3 bg-gradient-to-r from-yellow-300 from-10% via-rose-300 via-30% to-sky-500 to-90% p-6 text-BLACK dark:border-r justify-between">
         <div className="flex justify-center p-6">
           <Image
