3b704f90d259b8aa66c1ae01ce29b6d969623b13
breath-imager <14821226+breath-imager@users.noreply.github.com>
2024-11-13 12:30:57 +0300

    made static version of checkpoint list due to on chain verifcation hanging

--- app/checkpoints/page.tsx
+++ app/checkpoints/page.tsx
@@ -1,5 +1,14 @@
 import Checkpoints from "@/components/checkpoints";
 import Image from "next/image";
+  const checkpointNames = [
+    "Entrance checkpoint",
+    "Bar checkpoint",
+    "Ten by RARI exhibition checkpoint",
+    "Merch stand checkpoint",
+    "Bonus! Dancefloor checkpoint",
+  ];
+
+  const checkins = [false, false, false, false, false];
 
 export default async function CheckpointsPage() {
   return (
@@ -7,6 +16,33 @@ export default async function CheckpointsPage() {
       <div className="relative  flex flex-col gap-3 bg-gradient-to-r from-yellow-300 from-10% via-rose-300 via-30% to-sky-500 to-90% p-6 text-BLACK dark:border-r justify-between">
         <div className="flex justify-center p-6">
           <Checkpoints />
+        </div>
+      </div>
+      <div className="relative  flex flex-col gap-3 bg-gradient-to-r from-yellow-300 from-10% via-rose-300 via-30% to-sky-500 to-90% p-6 text-BLACK dark:border-r justify-between">
+        <div className="flex justify-center p-6">
+            <div className="flex flex-col text-xl gap-3 text-black">
+              {checkins?.map((checkin: boolean, index: number) => (
+                <div key={index} className="flex gap-2 items-center">
+                  
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="size-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                      />
+                    </svg>
+                  
+                  <p>{checkpointNames[index]}</p>
+                </div>
+              ))}
+            </div>
         </div>
       </div>
       <div className="relative  flex flex-col gap-3 bg-gradient-to-r from-yellow-300 from-10% via-rose-300 via-30% to-sky-500 to-90% p-6 text-BLACK dark:border-r justify-between">
--- components/checkpoints.tsx
+++ components/checkpoints.tsx
@@ -88,49 +88,7 @@ export default function Checkpoints() {
 
   return (
     <Auth>
-      <div className="flex flex-col text-xl gap-3 text-black">
-        {checkins?.map((checkin: boolean, index: number) => (
-          <div key={index} className="flex gap-2 items-center">
-            {!checkin ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 9.563C9 9.252 9.252 9 9.563 9h4.874c.311 0 .563.252.563.563v4.874c0 .311-.252.563-.563.563H9.564A.562.562 0 0 1 9 14.437V9.564Z"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="size-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-              </svg>
-            )}
-            <p>{checkpointNames[index]}</p>
-          </div>
-        ))}
-      </div>
+      &nbsp;
     </Auth>
   );
 }
