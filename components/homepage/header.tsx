1f59e7e4457c6081594e57ead91ebf5c71a10e34
David Hurley <dhurls99@gmail.com>
2025-01-08 21:40:19 -0500

    remove connect button from header

--- components/homepage/header.tsx
+++ components/homepage/header.tsx
@@ -1,10 +1,8 @@
-import { ConnectButton } from "../connect-button";
 import Image from "next/image";
 export const Header = () => {
   return (
-    <div className="flex justify-between items-center p-1 bg-gradient-to-b from-[#E3E3E3] to-[#E5E5E5] h-[80px]">
+    <div className="flex justify-center items-center p-1 bg-gradient-to-b from-[#E3E3E3] to-[#E5E5E5] h-[80px]">
       <Image src="/logo.svg" alt="Refraction" width={294} height={35} />
-      <ConnectButton />
     </div>
   );
 };
