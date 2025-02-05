22f6bd71fb13ffe7b4241f2f9cd0c2b5518cfbc7
David Hurley <dhurls99@gmail.com>
2025-02-04 21:23:12 -0500

    update copy

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
       <div className="p-4 sm:p-8 bg-black rounded-sm flex flex-col gap-8 md:gap-12 ">
         <div className="flex flex-col md:flex-row md:gap-12 gap-6">
           <h2 className="text-[#00E232] text-2xl sm:text-5xl w-full">
-            Developed by Refraction, a pioneer in web3 arts and culture, the IRL
+            {`Developed by Refraction, a pioneer in web3 arts and culture, the IRL
             protocol realizes the blockchain's potential to revolutionize the
-            creative industry.
+            creative industry.`}
           </h2>
           <div className="w-full md:w-3/5">
             <Image
