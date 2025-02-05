a43ffe6e9d284718f35a72ac35481a667cc9a125
David Hurley <dhurls99@gmail.com>
2025-02-05 14:30:49 -0500

    update copy

--- components/checkpoint.tsx
+++ components/checkpoint.tsx
@@ -17,27 +17,31 @@ const website = () => {
 };
 
 const checkInTitles = [
-  "Start Your IRL Side Quest",
+  "",
   "Welcome to Rendered Frequencies @ Ledger 106",
   "Ledger Stax Installation",
+  "Free from Compromise",
 ];
 
 const checkInSubtitles = ["", "", "by Emily Edelman"];
 const checkInMessages = [
-  "Let’s get started! Click the button below to learn more about the art exhibition and unlock the first checkpoint on your Side Quest.\n\nSide Quests are your opportunity to earn points on the IRL protocol ahead of the token launch in June 2025. \n\nIn partnership with Ledger and powered by Refraction's global network of artists, creatives and culture institutions, IRL bridges tangible and virtual worlds, forming the connective tissue between decentralized internet and lived reality.",
-  "Rendered Frequencies reimagines the encounter between art, music and audience by placing physicality and presence at the core of its ethos.\n\nDigital and physical experiences are no longer diametrically opposed— free from this compromise of deriving value from a singular realm, artists and audiences are encouraged to construct meaning across and between the increasingly indistinct digital-physical divide.\n\nThe exhibition, which takes place at Ledger’s newly unveiled and already iconic 106 HQ, features Anna Lucia, Fingacode, Kim Asendorf, Leander Herzog and Linda Dounia, exploring the essential role of seeing, experiencing, and connecting with art, music and beyond in real life. During the night of February 12th, Leander Herzog’s site specific installation of Heatsink, will be displayed in conversation with the music played throughout the evening.",
-  "Physicality becomes a defining theme in both form and function. Just as audiences are free from compromise when choosing between digital and physical experience, Ledger doubles down on this promise by building secure products that empower the protection of assets while providing an open platform for the sharing of creativity and culture.\n\nIn merging both worlds, a custom display of Ledger Stax’s builds into an object that serves as both a screen and a collectible artwork in its own right. Ledger Stax embodies the convergence of personalisation, uniqueness and materiality—offering not just a portal into the digital artwork but also a tangible artifact that carries its own distinct identity.",
+  "",
+  "Rendered Frequencies reimagines the encounter between art, music and audience by placing physicality and presence at the core of its ethos. \n\nDigital and physical experiences are no longer diametrically opposed— free from this compromise of deriving value from a singular realm, artists and audiences are encouraged to construct meaning across and between the increasingly indistinct digital-physical divide. \n\nThe exhibition, which takes place at Ledger’s newly unveiled and already iconic 106 HQ, features Anna Lucia, Fingacode, Kim Asendorf, Leander Herzog and Linda Dounia, exploring the essential role of seeing, experiencing, and connecting with art, music and beyond in real life. During the night of February 12th, Leander Herzog’s site specific installation of Heatsink, will be displayed in conversation with the music played throughout the evening.",
+  "Physicality becomes a defining theme in both form and function. Just as audiences are free from compromise when choosing between digital and physical experience, Ledger doubles down on this promise by building secure products that empower the protection of assets while providing an open platform for the sharing of creativity and culture. \n\nIn merging both worlds, a custom display of Ledger Stax’s builds into an object that serves as both a screen and a collectible artwork in its own right. Ledger Stax embodies the convergence of personalisation, uniqueness and materiality—offering not just a portal into the digital artwork but also a tangible artifact that carries its own distinct identity.",
+  "The importance of the IRL connection is not simply aesthetic but fundamental; it reaffirms art’s ability to anchor us in a tangible reality, where form, texture, and spatial presence can be fully felt. A dynamic choreography of objects, screens, and experiences—each element working to enhance the interplay between digital and physical realms. \n\nCheck-in at this Side Quest checkpoint to receive your unique code to claim your collectible embroidery patch, designed by Tabitha Swanson.",
 ];
 
 const checkedInTitles = [
+  "",
   "You’ve started your Side Quest!",
   "One step closer...",
   "You’re In!",
 ];
 
 const checkedInMessages = [
-  "Congratulations, you’ve checked in to your first Side Quest and have started earning IRL points.\n\nContinue to the back of the lobby to explore the bespoke Stax artwork by Emily Edelman and find your next Side Quest checkpoint.",
-  "Congratulations, you’ve checked in at the Ledger Stax Side Quest checkpoint and have earned more IRL points.\n\nIt’s time to head upstairs to the 8th floor— grab a drink and visit the vending machine to collect your Ledger collectible. ",
+  "",
+  "Congratulations, you’ve checked in to your first Side Quest and have started earning IRL points. Continue to the back of the lobby to explore the bespoke Stax artwork by Emily Edelman and find your next Side Quest checkpoint.",
+  "Congratulations, you’ve checked in at the Ledger Stax Side Quest checkpoint and have earned more IRL points. It’s time to head upstairs to the 8th floor— grab a drink and visit the vending machine to collect your Ledger collectible.",
   "Claim your physical collectible at the vending machine.",
 ];
 
@@ -48,8 +52,6 @@ export default function Checkpoint({ id }: CheckpointProps) {
   const { checkinStatus, setCheckinStatus } = useCheckInStatus(address, id);
   const [isCheckingIn, setIsCheckingIn] = useState(false);
   const router = useRouter();
-
-  console.log(checkinStatus);
 
   const handleCheckIn = async () => {
     setIsCheckingIn(true);
