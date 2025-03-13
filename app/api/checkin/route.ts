719426828c3076f9e99c2915d01e25b79d6cc8c8
Sam Mason de Caires <sam.jbmason@gmail.com>
2025-03-13 15:39:51 +0000

    Add determinstic uuid to stop duplicate tx's

--- app/api/checkin/route.ts
+++ app/api/checkin/route.ts
@@ -1,21 +1,25 @@
 import { NextRequest } from "next/server";
 import { SyndicateClient } from "@syndicateio/syndicate-node";
+import { v5 as uuidv5 } from 'uuid';
 
 import { checkinAddress } from "@/lib/checkin";
+import { DuplicateTransactionError } from "@syndicateio/syndicate-node/api/resources/transact";
 
 export async function POST(req: NextRequest) {
   const { walletAddress, checkpoint } = await req.json();
 
-  console.log(walletAddress, checkpoint);
+  // This is a random namespace UUID for the specific event, its only used so if the same wallet address is used for multiple events, it will generate a different UUID for each event
+  // This should be updated for each event if this code will be re-used across multiple events.
+  const EVENT_NAMESPACE = "4ed8f9e4-041d-437b-8509-cd4d8f15dea9"
+  const uuid = uuidv5(`${walletAddress}-${checkpoint}`, EVENT_NAMESPACE);
 
   try {
     const syndicate = new SyndicateClient({
       token: process.env.SYNDICATE_API_KEY as string,
     });
 
-    // TODO: the syndicate broadcasting wallet address would need to be allowlisted on the contract
-    // DEV: This likely wont work due to the current contract setup uses onlyOwner check
     const tx = await syndicate.transact.sendTransaction({
+      requestId: uuid,
       projectId: process.env.SYNDICATE_PROJECT_ID as string,
       contractAddress: checkinAddress,
       chainId: 63821,
@@ -34,6 +38,13 @@ export async function POST(req: NextRequest) {
     });
   } catch (e) {
     console.error(e);
+    // Returning a 200 here because the duplicate transaction error is expected
+    if(e instanceof DuplicateTransactionError) {
+      return new Response(JSON.stringify({ success: true }), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
     return new Response(JSON.stringify({ error: "An error occurred" }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
