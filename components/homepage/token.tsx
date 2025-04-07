8fa9d15d06aa99e7bc5839694b7817449accf86f
Breath Imager <14821226+breath-imager@users.noreply.github.com>
2025-04-07 18:06:39 -0400

    initial commit

--- components/homepage/token.tsx
+++ components/homepage/token.tsx
@@ -1,80 +1,47 @@
 "use client";
-import { getIPSFData } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
-import Creator from "./creator";
 import Link from "next/link";
 
-export const Token = ({
-  tokenId,
-  collectorClient,
-  tokenContract,
-}: {
-  tokenId: number;
-  collectorClient: any;
-  tokenContract: string;
-}) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState<Error | null>(null);
-  const [name, setName] = useState<string>("");
-  const [image, setImage] = useState<string>("");
-  const [creator, setCreator] = useState<`0x${string}` | null>(null);
+type Token = {
+  tokenAddress: string;
+  tokenId: string;
+  name: string;
+  image: string;
+  creator: string;
+  avatar: string;
+};
 
-  useEffect(() => {
-    const fetchTokens = async () => {
-      try {
-        const { token } = await collectorClient.getToken({
-          tokenContract,
-          tokenId,
-          mintType: "1155",
-        });
-        const tokenURI = await getIPSFData(
-          token.tokenURI.replace("ipfs://", "")
-        );
-        const name = tokenURI.name;
-        const image = `https://ipfs.io/ipfs/${tokenURI.image.replace(
-          "ipfs://",
-          ""
-        )}`;
-        setName(name.toUpperCase());
-        setImage(image);
-        setCreator(token.creator);
-        setIsLoading(false);
-      } catch (err) {
-        setError(err as Error);
-        setIsLoading(false);
-      }
-    };
-
-    fetchTokens();
-  }, []);
-
-  if (isLoading) return <div>Loading...</div>;
-  if (error) return <div>Error loading tokens</div>;
-
+export const Token = ({ token }: { token: Token }) => {
   return (
-    <div className="flex flex-col gap-3 bg-[#ffffff] p-4 sm:p-8 rounded-lg max-w-[600px] font-sans h-full">
+    <div className="flex flex-col gap-3 bg-[#ffffff]/50 p-4 sm:p-8 rounded-lg max-w-[600px] font-sans h-full">
       <div
         className="w-full aspect-square relative rounded-lg overflow-hidden"
         style={{
-          backgroundImage: `url(${image})`,
+          backgroundImage: `url(${token.image})`,
           backgroundPosition: "center",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
         }}
       />
-      <div className="text-2xl">{name}</div>
+      <div className="text-2xl">{token.name}</div>
 
-      {creator && <Creator creator={creator} />}
+      <span className="text-black flex items-center gap-2">
+        <img
+          src={token.avatar}
+          alt={token.creator}
+          className="w-4 h-4 rounded-full"
+        />
+        {token.creator.toUpperCase()}
+      </span>
       <div className="flex flex-row gap-6 justify-between mt-auto">
         <div className="flex">
           <Link
             target="_blank"
-            href={`https://zora.co/collect/base:${tokenContract}/${tokenId}?referrer=0xbD78783a26252bAf756e22f0DE764dfDcDa7733c`}
+            href={`https://zora.co/collect/base:${token.tokenAddress}/${token.tokenId}?referrer=0xbD78783a26252bAf756e22f0DE764dfDcDa7733c`}
           >
             <Button
               size="lg"
-              className="bg-gradient-to-r from-[#EE95BC] to-[#ED2D24]  inline-block text-black  uppercase  hover:bg-[#DDDDDD]/90  sm:w-auto"
+              className="bg-gradient-to-r from-[#EE95BC] to-[#ED2D24] inline-block text-black uppercase hover:bg-[#DDDDDD]/90  sm:w-auto"
             >
               Mint
             </Button>
