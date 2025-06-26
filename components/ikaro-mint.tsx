cad32886802db4d5c815f320fa36d15fecfd256c
breath-imager <14821226+breath-imager@users.noreply.github.com>
2025-06-26 11:46:02 -0400

    added socials for ikaro

--- components/ikaro-mint.tsx
+++ components/ikaro-mint.tsx
@@ -466,6 +466,34 @@ export default function IkaroMint() {
                       </svg>
                     </a>
                   </p>
+                  <p className="text-lg font-grotesk leading-relaxed">
+                    <span className="text-[11px] flex items-center gap-1">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+                      </svg>
+                      FOLLOW
+                    </span><br/>
+                    <div className="flex gap-2 mt-2">
+                      <a href="https://twitter.com/occult3d" target="_blank" rel="noopener noreferrer" className="flex items-center justify-between bg-gray-900/10 text-black rounded-full px-4 py-2 hover:bg-gray-100">
+                        <span>Twitter</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-2" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
+                      </a>
+                      <a href="https://www.instagram.com/occulted" target="_blank" rel="noopener noreferrer" className="flex items-center justify-between bg-gray-900/10 text-black rounded-full px-4 py-2 hover:bg-gray-100">
+                        <span>Instagram</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-2" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
+                      </a>
+                       <a href="https://www.occulted.xyz" target="_blank" rel="noopener noreferrer" className="flex items-center justify-between bg-gray-900/10 text-black rounded-full px-4 py-2 hover:bg-gray-100">
+                        <span>Website</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-2" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
+                      </a>
+                    </div>
+                  </p>
                 </>
               ) : (
                 <>
@@ -487,6 +515,34 @@ export default function IkaroMint() {
                         <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
                       </svg>
                     </a>
+                  </p>
+                  <p className="text-lg font-grotesk leading-relaxed">
+                    <span className="text-[11px] flex items-center gap-1">
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
+                        <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+                      </svg>
+                      FOLLOW
+                    </span><br/>
+                    <div className="flex gap-2 mt-2">
+                      <a href="https://twitter.com/occult3d" target="_blank" rel="noopener noreferrer" className="flex items-center justify-between bg-gray-900/10 text-black rounded-full px-4 py-2 hover:bg-gray-100">
+                        <span>Twitter</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-2" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
+                      </a>
+                      <a href="https://www.instagram.com/occulted" target="_blank" rel="noopener noreferrer" className="flex items-center justify-between bg-gray-900/10 text-black rounded-full px-4 py-2 hover:bg-gray-100">
+                        <span>Instagram</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-2" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
+                      </a>
+                       <a href="https://www.occulted.xyz" target="_blank" rel="noopener noreferrer" className="flex items-center justify-between bg-gray-900/10 text-black rounded-full px-4 py-2 hover:bg-gray-100">
+                        <span>Website</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-2" viewBox="0 0 20 20" fill="currentColor">
+                          <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" />
+                        </svg>
+                      </a>
+                    </div>
                   </p>
                 </>
               )}
