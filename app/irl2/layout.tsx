a391179fa6fa7a2c576b4f2daa7963668bdc1a13
breath-imager <14821226+breath-imager@users.noreply.github.com>
2025-07-24 23:56:11 -0400

    new ui

---
+++ app/irl2/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Toaster } from "@/components/ui/toaster";
+import Image from "next/image";
+import Link from "next/link";
+import Auth from "@/components/ikaro-auth";
+import Header from "@/components/ikaro-header";
+
+export const metadata: Metadata = {
+  title: "$IRL",
+  description: "$IRL is your key to unlocking a new way to experience culture",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen">
+      <div
+        className={`flex flex-col gap-6 lg:p-6 p-2  bg-[linear-gradient(to_bottom,#EC9DC6_0%,#5FBECC_25%,#FFE600_50%,#F24508_100%)]`}
+      >
+        <div className="flex justify-between items-center px-2 lg:px-4">
+          <Link href="/">
+            <Image 
+              src="/images/ikaro/irl-logo.png" 
+              alt="IRL" 
+              width={40} 
+              height={40} 
+              className="w-10 h-10"
+            />
+          </Link>
+          <Header />
+        </div>
+        <Auth>{children}</Auth>
+      </div>
+      <Toaster />
+    </div>
+  );
+}
