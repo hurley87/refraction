d3d0963b7d5ebe8368676d6b6c2d895970f0eae7
breath-imager <14821226+breath-imager@users.noreply.github.com>
2025-08-14 10:29:42 -0400

    made the checkpoint title bilingual depending on toggle value

--- lib/mutek.ts
+++ lib/mutek.ts
@@ -3,6 +3,7 @@ export const mutek = [
     checkpoint: "1",
     image: "/mutek/1.jpg",
     title: "IRL Scavenger Hunt",
+    title_fr: "CHASSE AU TRÉSOR IRL",
     subtitle: "S.A.T",
     content:
       "Welcome to the IRL Scavenger Hunt at 2025's Villlage Numérique presented by MUTEK and Xn Québec. Powered by Refraction, IRL is culture's rewards program, rewarding participation in cultural experiences, from events to creative contributions. \n\nYou've checked in at the SAT and are already earning IRL points, redeemable for access, merch, and more. Check-in at all 5 IRL checkpoints across the Village Numérique for your chance to win a MUTEK 2026 passport. \n\n Artworks to view nearby:  \nThe Door of the Refuge, Normal Studio, \nAstronomical water, Martin Messier \nVS AI Street Fighting, Dimension Plus\n\n",
@@ -12,6 +13,7 @@ export const mutek = [
     checkpoint: "2",
     image: "/mutek/2.jpg",
     title: "IRL Scavenger Hunt",
+    title_fr: "CHASSE AU TRÉSOR IRL",
     subtitle: "Espace Innovation",
     content:
       "Welcome to the IRL Scavenger Hunt at 2025's Villlage Numérique presented by MUTEK and Xn Québec. Powered by Refraction, IRL is culture's rewards program, rewarding participation in cultural experiences, from events to creative contributions. \n\nYou've checked in at Espace innovation and are already earning IRL points, redeemable for access, merch, and more. Check-in at all 5 IRL checkpoints across the Village Numérique for your chance to win a MUTEK 2026 passport. \n\nArtworks to view nearby:  \nSimUlator, Amélie Barrette and Laurence Wells \nKatsi'noniowá:nen - The Flying Head, Kahentawaks Tiewishaw \nOrange Pekoe, Vanessa Racine \nChamberS, Jihen Ben Chikha \nCLTRS, Laureline Chiapello \nPor si acaso se acaba el mundo todo el sol he de aprovechar, Sabina Gámez\n\n",
@@ -20,8 +22,9 @@ export const mutek = [
   },
   {
     checkpoint: "3",
-    image: "/mutek/3.jpg",
+    image: "/mutek/3.jpg",  
     title: "IRL Scavenger Hunt",
+    title_fr: "CHASSE AU TRÉSOR IRL",
     subtitle: "Le Parterre",
     content:
       "Welcome to the IRL Scavenger Hunt at 2025's Villlage Numérique presented by MUTEK and Xn Québec. Powered by Refraction, IRL is culture's rewards program, rewarding participation in cultural experiences, from events to creative contributions. \n\nYou've checked in at Le Parterre and are already earning IRL points, redeemable for access, merch, and more. Check-in at all 5 IRL checkpoints across the Village Numérique for your chance to win a MUTEK 2026 passport. \n\nArtworks to view at this location:  \nIn camera, Ying Gao \nI'M NOT A ROBOT, Cadie Desbiens-Desmeules \nTETRA, Ottomata \nSituational Compliance, Matthew Biederman and Lucas Paris \nPublic Space, Latent Space, Danny Perreault and Victor Drouin-Trempe \nFor You I Will Be An Island, Chun Hua Catherine Dong \nFLIP!, Troublemakers \nHEXAPHONE, Troublemakers \n\n",
@@ -31,7 +34,8 @@ export const mutek = [
   {
     checkpoint: "4",
     image: "/mutek/4.jpg",
-    title: "IRL Scavenger Hunt",
+    title: "IRL Scavenger Hunt",  
+    title_fr: "CHASSE AU TRÉSOR IRL",
     subtitle: "Agora, Coeur des sciences (UQAM)",
     content:
       "Welcome to the IRL Scavenger Hunt at 2025's Villlage Numérique presented by MUTEK and Xn Québec. Powered by Refraction, IRL is culture's rewards program, rewarding participation in cultural experiences, from events to creative contributions. \n\nYou've checked in at Agora, Coeur des sciences (UQAM) and are already earning IRL points, redeemable for access, merch, and more. Check-in at all 5 IRL checkpoints across the Village Numérique for your chance to win a MUTEK 2026 passport. \n\nArtworks to view nearby:  \nÉco-sonorités du vivant, Victor Drouin-Trempe \nStorms, Quayola \nOPAL, Gentilhomme \nNEST: Colony, Iregular \nDialogues, Isotone \n\n",
@@ -40,7 +44,8 @@ export const mutek = [
   {
     checkpoint: "5",
     image: "/mutek/5.jpg",
-    title: "IRL Scavenger Hunt",
+    title: "IRL Scavenger Hunt",  
+    title_fr: "CHASSE AU TRÉSOR IRL",
     subtitle: "Place des Arts",
     content:
       "Welcome to the IRL Scavenger Hunt at 2025's Villlage Numérique presented by MUTEK and Xn Québec. Powered by Refraction, IRL is culture's rewards program, rewarding participation in cultural experiences, from events to creative contributions. \n\nYou've checked in at Place des Arts and are already earning IRL points, redeemable for access, merch, and more. Check-in at all 5 IRL checkpoints across the Village Numérique for your chance to win a MUTEK 2026 passport. \n\nArtworks to view at this location:  \nLabyrinthe, students from UQAM’s School of Visual and Media Arts \nReflections, fig55 \n\n",
