c898152cb45cf334e542944816cf26ac39d97ecf
David Hurley <dhurls99@gmail.com>
2026-01-10 11:25:02 -0500

    refactor: Remove obsolete components to streamline the codebase
    Deleted the following unused components:
    - components/challenges-page.tsx
    - components/countdown.tsx
    - components/editorial.tsx
    - components/email-template.tsx
    - components/success-animation.tsx
    - components/auth/connect-button.tsx
    - components/auth/export-wallet-button.tsx
    - components/auth/logout.tsx
    - components/homepage/coin.tsx
    - components/homepage/creator.tsx
    - components/homepage/event.tsx
    - components/homepage/events.tsx
    - components/homepage/header.tsx
    - components/homepage/token.old
    - components/homepage/token.tsx
    - components/homepage/tokens.tsx
    - components/perks/points-activities-guide.tsx
    - components/stellar/test-contract-call.tsx
    
    Key changes:
    - Enhanced maintainability by removing outdated and unused components from the codebase.

--- components/homepage/header.tsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/image";
-export const Header = () => {
-  return (
-    <div className="flex justify-center items-center p-1 bg-gradient-to-b from-[#E3E3E3] to-[#E5E5E5] h-[80px]">
-      <Image src="/logo.svg" alt="Refraction" width={294} height={35} />
-    </div>
-  );
-};
